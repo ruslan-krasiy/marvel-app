bf7f922627928f1c32eca1ed2576da49e22d002f
ruslan.krasiy <ruslan.krasiy@alten.es>
2024-02-28 16:49:42 +0100

    Refs #1 Add router
        - Done

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,28 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Layout from "./layout";
+import Heroes from "./views/Heroes";
+import Hero from "./views/Hero";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout />,
+    children: [
+      { index: true, element: <Heroes /> },
+      { path: "hero", children: [
+        {path: ":id", element: <Hero /> }]}
+    ],
   },
+  {
+    path: "*",
+    element: <Navigate to={"/"} />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
---
+++ src/views/Hero.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Hero:FC = () => {
+  return (
+    <div>Hero Page</div>
+  );
+};
+
+export default Hero;
